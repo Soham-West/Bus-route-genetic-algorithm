b487b53384560af2bec62247f1b0f831fd128659
Soham Patil <sohampatilwest@gmail.com>
2022-08-12 14:17:58 -0400

    Added first part oh the school and format bus grid

--- bus_grid_env.py
+++ bus_grid_env.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from collections import Counter
+import math
 #Create 4 by 4 grid
 
 num_of_stops = 4
 x = []
 y = []
-for s in range(0, num_of_stops):
+for s in range(0, num_of_stops + 1):
     o = random.uniform(0, num_of_stops)
     h = random.uniform(0, num_of_stops)
     if (o.is_integer() is False) and (h.is_integer() is False):
@@ -19,25 +21,44 @@ for s in range(0, num_of_stops):
     y.append(h)
 print(x)
 print(y)
-annotations = ["A", "B", "C", "D"]
 
-line1 = plt.plot([x], [y], "ko")
+annotations = ["School", "A", "B", "C", "D"]
+
+line1 = plt.plot([x], [y], "--ko", markersize = 5)
 
 for a, label in enumerate(annotations):
     plt.annotate(label, (x[a], y[a]))
 
 
-plt.axis([0, 4, 0, 4])
+count_x = Counter(x)
+count_y = Counter(y)
+
+result_x = [i for i, j in count_x.items() if j > 1]
+result_y = [i for i, j in count_y.items() if j > 1]
+
+for x_cord, y_cord in zip(x, y):
+    plt.text(x_cord, y_cord + 0.15, '({}, {})'.format(x_cord, y_cord))
+    if result_x != []:
+        
+    
+
+print(count_x)
+print(count_y)
+print(result_x)
+print(result_y)
+
+axis = np.array([0, 4, 0 ,4])
+plt.axis(axis)
 
 l = line1.pop(0)
 l.remove()
 
-plt.xticks(range(0, num_of_stops + 1))
-plt.yticks(range(0, num_of_stops + 1))
+plt.xticks(range(0, num_of_stops))
+plt.yticks(range(0, num_of_stops))
 
 plt.xlabel("Latitude")
 plt.ylabel("Longitude")
 
 plt.tight_layout()
 plt.grid()
-plt.show()
+#plt.show()
