77b3efd55ff3c760e7aa32807a206b7f07b7f750
Soham Patil <sohampatilwest@gmail.com>
2022-08-30 16:50:30 -0400

    better grid and bfs+dfs

--- branch-and-bound-algorithm.py
+++ branch-and-bound-algorithm.py
@@ -1,34 +1,34 @@
 from bus_grid_env import *
-print(((num_of_gridlines + 1)**2) - num_of_gridlines)
+#print(((num_of_gridlines + 1)**2) - num_of_gridlines)
 ##print(y_cords)
 def make_matrix(len_of_side):
     len_of_side = ((num_of_gridlines + 1)**2)
     graph = {}
     for i in range(1, len_of_side + 1):
         if i == 1:
-            graph[i] = [i + 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i + 1), str(i + num_of_gridlines + 1)]
         elif i == num_of_gridlines + 1:
-            graph[i] = [i - 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i - 1), str(i + num_of_gridlines + 1)]
         elif i == len_of_side - num_of_gridlines:
-            graph[i] = [i - (num_of_gridlines + 1), i + 1]
+            graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i + 1)]
         elif i == len_of_side:
-            graph[i] = [i - (num_of_gridlines + 1), i - 1]
+            graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i - 1)]
         elif 1 < i < num_of_gridlines + 1:
-            graph[i] = [i - 1, i + 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i - 1), str(i + 1), str(i + num_of_gridlines + 1)]
         elif (i - 1)%(num_of_gridlines + 1) == 0:
-            graph[i] = [i - (num_of_gridlines + 1), i + 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i + 1), str(i + num_of_gridlines + 1)]
         elif i%(num_of_gridlines + 1) == 0:
-            graph[i] = [i - (num_of_gridlines + 1), i - 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i - 1), str(i + num_of_gridlines + 1)]
         elif (len_of_side - num_of_gridlines) < i < len_of_side:
-            graph[i] = [i + num_of_gridlines + 1, i - 1, i + 1]
+            graph[str(i)] = [str(i + num_of_gridlines + 1), str(i - 1), str(i + 1)]
         else:
-            graph[i] = [i - (num_of_gridlines + 1), i - 1, i + 1, i + num_of_gridlines + 1]
+            graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i - 1), str(i + 1), str(i + num_of_gridlines + 1)]
+        #graph[i] = [i]
+    return graph   
         
 
-
-    #graph[i] = [i]
-    print(graph)
 graph = make_matrix(num_of_gridlines)
+
 """graph = {
         "A": ["B", "F"],
         "B": ["A", "C", "G"],
@@ -57,24 +57,45 @@ graph = make_matrix(num_of_gridlines)
         "Y": ["T", "X"]
         }"""
 
-def bfs(graph, start, end):
+        
+
+def bfs(graph, p_start, p_end):
     queue = []
-    queue.append([start])
+
+    queue.append([p_start])
+    visited = set()
     while queue:
         path = queue.pop(0)
         node = path[-1]
-        # path found
-        if node == end:
+        if node in visited:
+            continue
+        if node == p_end:
+            #print(len(path))
+            r_path = []
+            for direction in range(1, (len(new_path) - 1)):
+                if int(new_path[direction]) + 1 == int(new_path[direction + 1]):
+                    r_path.append("R")
+                if int(new_path[direction]) + num_of_gridlines + 1 == int(new_path[direction + 1]):
+                    r_path.append("D")
+                if int(new_path[direction]) - 1 == int(new_path[direction + 1]):
+                    r_path.append("L")
+                if int(new_path[direction]) - (num_of_gridlines + 1) == int(new_path[direction + 1]):
+                    r_path.append("U")
+
+            print(r_path)
             return path
-        print(graph.get(node, []))
-        for adjacent in graph.get(node, []):
-            new_path = list(path)
-            new_path.append(adjacent)
-            queue.append(new_path)
-        for direction in "U", "R", "D", "L":
-            branch = object()
+            #return new_path[0]
+        else:
+            for adjacent in graph.get(node, []):
+                if adjacent not in visited:
+                    new_path = list(path)
+                    new_path.append(adjacent)
+                    queue.append(new_path)
+            
+
+        
 
 
   
-#print(bfs(graph, "A", "Y"))
+print(bfs(graph, "1", "15"))
 
