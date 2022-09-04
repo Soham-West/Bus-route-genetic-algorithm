ec54929ea649d7ae72b7636b63fc32adb62f5245
Soham-West <110839578+Soham-West@users.noreply.github.com>
2022-09-04 17:09:47 -0400

    Update branch-and-bound-algorithm.py

--- branch-and-bound-algorithm.py
+++ branch-and-bound-algorithm.py
@@ -25,7 +25,7 @@ def make_matrix(len_of_side):
         else:
             graph[str(i)] = [str(i - (num_of_gridlines + 1)), str(i - 1), str(i + 1), str(i + num_of_gridlines + 1)]
         #graph[i] = [i]
-    
+    #print(graph)
     return graph   
         
 
@@ -66,22 +66,77 @@ def get_nearest_node(point1_x, point1_y, point2_x, point2_y):
     y_2 = float(point2_y)
     if y_1.is_integer() == True and x_2 > x_1:
         s_path.append("R")
-        t = math.floor(x_1 + 1.0)
-        z = [t, y_1]
-        print(z)
+        t = math.floor(int(x_1) + 1.0)
+        z = [t, int(y_1)]
     elif y_1.is_integer() == True and x_2 < x_1:
         s_path.append("L")
-        t = math.ceil(x_1 - 1.0)
-
+        t = math.ceil(int(x_1) - 1.0)
+        z = [t, int(y_1)]
     elif x_1.is_integer() == True and y_2 > y_1:
         s_path.append("U")
+        t = math.floor(y_1 + 1.0)
+        z = [int(x_1), t]
     elif x_1.is_integer() == True and y_2 < y_1:
         s_path.append("D")
-    print(s_path)
+        t = math.ceil(y_1 - 1.0)
+        z = [int(x_1), t]
+    pp = 1
+    p1_x = z[0]
+    p1_y = (num_of_gridlines - z[1]) * (num_of_gridlines + 1)
+    group = p1_x + p1_y
+    pp += group
+    return str(pp)
+
+
     
-get_nearest_node(x_cords[0], y_cords[0], x_cords[1], y_cords[1])
-print(used_annotations[0], used_annotations[1])
-plt.show()
+
+start_point = (get_nearest_node(x_cords[0], y_cords[0], x_cords[1], y_cords[1]))
+
+
+#end_point = str(get_nearest_node(x_cords[1], y_cords[1], x_cords[0], y_cords[0]))
+#print(start_point)
+
+#print(end_point)
+print(used_annotations[0])
+print(used_annotations[1])
+stop_break = 0
+#if start_point == end_point:
+   #while stop_break == 0:
+    #break
+#print(int(i))
+l_path = []
+def end_node(p1x, p1y, p2x, p2y):
+    x_1 = float(p1x)
+    x_2 = float(p2x)
+    y_1 = float(p1y)
+    y_2 = float(p2y)
+    if y_1.is_integer() == True and x_2 > x_1:
+        t = math.floor(int(x_1) + 1.0)
+        z = [t, int(y_1)]
+    elif y_1.is_integer() == True and x_2 < x_1:
+        t = math.ceil(int(x_1) - 1.0)
+        z = [t, int(y_1)]
+    elif x_1.is_integer() == True and y_2 > y_1:
+        t = math.floor(y_1 + 1.0)
+        z = [int(x_1), t]
+    elif x_1.is_integer() == True and y_2 < y_1:
+        t = math.ceil(y_1 - 1.0)
+        z = [int(x_1), t]
+    n = 1
+    p1_x = z[0]
+    p1_y = (num_of_gridlines - z[1]) * (num_of_gridlines + 1)
+    group = p1_x + p1_y
+    n += group
+    if z[0] < p1x and z[1] == p1y:
+        l_path.append("R")
+    elif z[0] > p1x and z[1] == p1y:
+        l_path.append("L")
+    elif z[1] < p1y and z[0] == p1x:
+        l_path.append("U")
+    elif z[1] < p1y and z[0] == p1x:
+        l_path.append("D")
+    return str(n)
+
 
 
 def bfs(graph, p_start, p_end):
@@ -94,31 +149,32 @@ def bfs(graph, p_start, p_end):
         if node in visited:
             continue
         if node == p_end:
-            #print(len(path))
-            r_path = []
-            for direction in range(1, (len(new_path) - 1)):
-                if int(new_path[direction]) + 1 == int(new_path[direction + 1]):
-                    r_path.append("R")
-                if int(new_path[direction]) + num_of_gridlines + 1 == int(new_path[direction + 1]):
-                    r_path.append("D")
-                if int(new_path[direction]) - 1 == int(new_path[direction + 1]):
-                    r_path.append("L")
-                if int(new_path[direction]) - (num_of_gridlines + 1) == int(new_path[direction + 1]):
-                    r_path.append("U")
-
-            print(r_path)
-            return path
+           return path
         else:
             for adjacent in graph.get(node, []):
                 if adjacent not in visited:
                     new_path = list(path)
                     new_path.append(adjacent)
                     queue.append(new_path)
-          
 
-        
+r_path = []
+r_path.append(s_path[0])
+end_point = end_node(x_cords[1], y_cords[1], x_cords[0], y_cords[0])
+path_1 = bfs(graph, start_point, end_point)
+path = path_1
 
+for direction in range(0, (len(path) - 1)):
 
-  
-print(bfs(graph, "1", "13"))
+    if int(path[direction]) + 1 == int(path[direction + 1]):
+        r_path.append("R")
+    elif int(path[direction]) - (num_of_gridlines + 1) == int(path[direction + 1]):
+        r_path.append("U")
+    elif int(path[direction]) - 1 == int(path[direction + 1]):
+        r_path.append("L")
+    elif int(path[direction]) + (num_of_gridlines + 1) == int(path[direction + 1]):
+        r_path.append("D")
+r_path.append(l_path[0])
+print(r_path)
+print(path)
 
+plt.show()
