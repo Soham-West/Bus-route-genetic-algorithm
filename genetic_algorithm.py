a7398549d2e8cfb08e6f35fa007f5aee119f4ca0
Soham-West <110839578+Soham-West@users.noreply.github.com>
2022-10-02 14:45:33 -0400

    Update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -7,12 +7,24 @@ def time(gen):
     return distance
 #time(["A", "B", "C", "D"])
 
-num_of_generations = 10
-gen_size = 10
+def mutate(gen):
+    change_index_1 = random.randint(1, len(gen) - 2)
+    change_index_2 = random.randint(1, len(gen) - 2)
+    if change_index_1 == change_index_2:
+        change_index_2 = random.randint(1, len(gen) - 2)
+    stop_1 = gen[change_index_1]
+    stop_2 = gen[change_index_2]
+    gen[change_index_2] = stop_1
+    gen[change_index_1] = stop_2
+    return gen
+
+num_of_generations = 7
+gen_size = 7
 generation_population = []
-best_route, best_score = annotations_2, 100
+best_route, best_score = annotations_2, 1000000
 for x in range(gen_size):
-    population = random.sample(annotations_2, num_of_stops)
+    population_0 = random.sample(annotations_2, num_of_stops)
+    population = ["School"] + population_0 + ["School"]
     generation_population.append(population)
     
 
@@ -29,17 +41,26 @@ for gen in range(num_of_generations):
     children = []
 
     children.append(parent)
-
-    for i in range(1, gen_size):
-        child = random.sample(annotations_2, num_of_stops)
+    child_1 = mutate(parent)
+    child_2 = mutate(parent)
+    children.append(child_1)
+    children.append(child_2)
+    for i in range(3, gen_size):
+        child_0 = random.sample(annotations_2, num_of_stops)
+        child = ["School"] + child_0 + ["School"]
         children.append(child)
 
     generation_population = children
 print(best_route)
 print(best_score)
 for i in range(0, len(best_route) - 1):
-    print(fastest_2_points(best_route[i], best_route[i + 1])[2])
+    point1x = fastest_2_points(best_route[i], best_route[i + 1])[3]
+    point1y = fastest_2_points(best_route[i], best_route[i + 1])[4]
+    point2x = fastest_2_points(best_route[i], best_route[i + 1])[5]
+    point2y = fastest_2_points(best_route[i], best_route[i + 1])[6]
+    path = fastest_2_points(best_route[i], best_route[i + 1])[1]
+    draw_graph(path, point1x, point1y, point2x, point2y)
 
 #draw_graph(path, [])
 #print(generation_population)
-#plt.show()
+plt.show()
